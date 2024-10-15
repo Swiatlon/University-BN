2936c459295a98c2865fdc87e248989a45d58e92
Swiatlon <przemyslaw.swiatlon@gmail.com>
2024-10-15 21:04:46 +0200

    Fixed built typeorm database config

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 import { SeedersClasses } from '@seeds/seeds';
 dotenv.config();
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, NODE_ENV } = process.env;
+
+const isDevelopment = NODE_ENV !== 'PRODUCTION';
 
 const options: DataSourceOptions & SeederOptions = {
     type: 'postgres',
@@ -16,13 +18,13 @@ const options: DataSourceOptions & SeederOptions = {
     synchronize: false,
     logging: false,
     logger: 'advanced-console',
-    entities: [`${__dirname}/../entities/**/*.ts`],
-    migrations: [`${__dirname}/../migrations/*.ts`],
+    entities: [`${__dirname}/../entities/**/*${isDevelopment ? '.ts' : '.js'}`],
+    migrations: [`${__dirname}/../migrations/**/*${isDevelopment ? '.ts' : '.js'}`],
     seeds: SeedersClasses,
     ssl: {
         rejectUnauthorized: true,
     },
-    factories: [`${__dirname}/../factories/**/*.ts`],
+    factories: [`${__dirname}/../factories/**/*${isDevelopment ? '.ts' : '.js'}`],
 };
 
 export const AppDataSource = new DataSource(options);
