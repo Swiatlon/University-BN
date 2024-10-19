fbcc11c95cbf09f789a05f4945994aad6cba7b1e
unknown <wiercikdoc@gmail.com>
2024-10-19 10:32:42 +0200

    Removed old code

--- src/controllers/UserInfo.Controller.ts
+++ src/controllers/UserInfo.Controller.ts
@@ -9,8 +9,6 @@ const userInfoServices = new UserInfoService();
 
 const getUserInfo = asyncHandler(async (req: ICustomVisbilityFieldRequest, res: Response) => {
     const userInfo = req.cookies.userInfo as IUserInfo;
-    console.log('cookies', req.cookies);
-
     const userData = await userInfoServices.getUserInfo(userInfo);
 
     res.status(HTTP_STATUS.OK.code).json(userData);
